50e01c3ab90375fd277be4d77fd3301547071309
Andrey Novikov <a.novikov@stacktelecom.ru>
2024-10-07 14:27:38 +0300

    refactor to async transcriber service

---
+++ transcriber_async.py
@@ -0,0 +1,57 @@
+import os
+import wave
+import json
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from vosk import Model, KaldiRecognizer
+
+class Transcriber:
+    def __init__(self, model_path):
+        self.model = Model(model_path)
+        self.executor = ThreadPoolExecutor()
+
+    async def transcribe_audio(self, audio_file_path):
+        loop = asyncio.get_event_loop()
+        return await loop.run_in_executor(self.executor, self._transcribe_audio_sync, audio_file_path)
+
+    def _transcribe_audio_sync(self, audio_file_path):
+        wf = wave.open(audio_file_path, "rb")
+
+        if wf.getnchannels() != 1:
+            raise ValueError("Аудиофайл должен быть моно!")
+        rec = KaldiRecognizer(self.model, wf.getframerate())
+        rec.SetWords(True)
+
+        result = []
+        while True:
+            data = wf.readframes(8000)
+            if len(data) == 0:
+                break
+            if rec.AcceptWaveform(data):
+                result.append(json.loads(rec.Result()))
+            else:
+                result.append(json.loads(rec.PartialResult()))
+
+        result.append(json.loads(rec.FinalResult()))
+        wf.close()
+        return result
+
+    async def write_transcription_to_file(self, audio_file_path, output_file_path):
+        transcription = await self.transcribe_audio(audio_file_path)
+        loop = asyncio.get_event_loop()
+        await loop.run_in_executor(self.executor,
+                                    self._write_transcription_to_file_sync,
+                                    transcription,
+                                    audio_file_path,
+                                    output_file_path)
+
+    def _write_transcription_to_file_sync(self,
+                                         transcription,
+                                         audio_file_path,
+                                         output_file_path):
+        base_name = os.path.splitext(os.path.basename(audio_file_path))[0]
+        output_file_path = f'{output_file_path}\{base_name}.txt'
+        with open(output_file_path, "w") as f:
+            for res in transcription:
+                if 'text' in res:
+                    f.write(res['text'] + "\n")
--- transcriber_service.py
+++ transcriber_service.py
@@ -2,24 +2,28 @@ import asyncio
 import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from transcriber_async import Transcriber
 
 
 # Класс для отслеживания появления новых файлов
-class NewFileHandler(FileSystemEventHandler):
-    def __init__(self, folder_path: str, loop):
+class AudioFileHandler(FileSystemEventHandler):
+    def __init__(self, folder_path: str, folder_output: str, loop):
         self.folder_path = folder_path
+        self.folder_output = folder_output
         self.loop = loop
+        self.transcriber = Transcriber('model')
 
     def on_created(self, event):
         if not event.is_directory and event.src_path.endswith(".wav"):
             print(f"Новый файл обнаружен: {event.src_path}")
-            asyncio.run_coroutine_threadsafe(self.file_handler(event.src_path), self.loop)
+            asyncio.run_coroutine_threadsafe(self.transcriber.write_transcription_to_file(event.src_path,
+                                                                          self.folder_output),
+                                                                          self.loop)
 
-    async def file_handler(self, file_path: str):
-        print(f"Executing file: {file_path}")
 
-def start_watching(folder_path: str, loop):
-    event_handler = NewFileHandler(folder_path, loop)
+
+def start_watching(folder_path: str, folder_output: str, loop):
+    event_handler = AudioFileHandler(folder_path, folder_output, loop)
     observer = Observer()
     observer.schedule(event_handler, folder_path, recursive=False)
     observer.start()
@@ -27,10 +31,12 @@ def start_watching(folder_path: str, loop):
 
 async def main():
     folder_to_watch = "./recorders"  # Папка для отслеживания
+    folder_output = "./transcriptions"  # Папка для сохранения text"
     os.makedirs(folder_to_watch, exist_ok=True)
+    os.makedirs(folder_output, exist_ok=True)
 
     loop = asyncio.get_running_loop()
-    observer = start_watching(folder_to_watch, loop)
+    observer = start_watching(folder_to_watch, folder_output, loop)
 
     try:
         while True:
@@ -39,5 +45,8 @@ async def main():
         observer.stop()
         observer.join()
 
+    
+
 if __name__ == "__main__":
+
     asyncio.run(main())
